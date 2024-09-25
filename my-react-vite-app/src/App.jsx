1019ce14349df2f34d16433c5c8fdb08eb3d6de5
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-09-25 18:50:12 +0300

    --

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -3,37 +3,6 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    axios("https://randomuser.me/api").then((res) => console.log(res.data));
-  });
-
-  useEffect(() => {
-    fetch("https://randomuser.me/api")
-      .then((res) => res.json())
-      .then((json) => console.log(json));
-  });
-
-  const obj = {
-    one: "one",
-    two: "two",
-  };
-
-  const obj2 = {
-    three: "three",
-    four: "four",
-  };
-
-  const arr = [1, 2, 3, 4, 5];
-  const a = "a";
-
-  const all = {
-    ...obj,
-    ...arr,
-    ...obj2,
-    a,
-  };
-
-  console.log(all);
   return <>Hello World!!!</>;
 }
 
