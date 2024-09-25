fa7fd78c8e9df5e9bdeb84e811747b45b52b2d4d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-09-25 19:22:55 +0300

    Update App.jsx

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -1,22 +1,6 @@
 import "./App.css";
 
 function App() {
-  const messages = [
-    "JavaScript",
-    (message) => console.log(message),
-    "Typescript",
-    (message) => console.log(message),
-    "React",
-    (message) => console.log(message),
-    "Redux",
-    (message) => console.log(message),
-  ];
-
-  messages[1](messages[0]);
-  messages[3](messages[2]);
-  messages[5](messages[4]);
-  messages[7](messages[6]);
-
   const insideFN = (logger) => logger("REACT");
 
   insideFN((message) => console.log(message));
