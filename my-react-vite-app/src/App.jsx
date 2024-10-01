b44ac2fe82c0d0ed4e811525a44c555188275f6d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-01 11:14:52 +0300

    Update App.jsx

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -4,10 +4,16 @@ function App() {
   const string = "Restaurants in Hanalei";
   let urlFriendly = "";
 
-  for 
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === " ") {
+      urlFriendly += "-";
+    } else {
+      urlFriendly += string[i];
+    }
+  }
 
-  const string2 = "Restaurants in Hanalei";
-
+  const string2 = "Restaurants2 in2 Hanalei2";
+  const urlFriendly2 = string2.replace(/ /g, "-");
 
   console.log(urlFriendly);
   console.log(urlFriendly2);
