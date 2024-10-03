17f94699cdc7200da5a84b4324bed582895a0b9a
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-03 22:07:37 +0300

    Update App.jsx

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -7,8 +7,14 @@ function App() {
     fast: true,
   };
 
-  const reFrederick = (person) => ({});
+  const reFrederick = (person) => ({
+    ...person,
+    strong: false,
+    fast: false,
+  });
 
+  console.log(frederick);
+  console.log(reFrederick(frederick));
   return <>O.REilLY-REACT</>;
 }
 
