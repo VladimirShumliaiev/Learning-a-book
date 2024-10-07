34ac61551700729db83b2f4c74f9315c0aa93b00
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-07 20:13:50 +0300

    Update App.jsx

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   const list = [{ name: "vasya" }, { name: "petya" }, { name: "misha" }];
 
-  const x = (name, list) => [...list, { name }];
+  const list2 = [{ name: "igor" }, { name: "misha" }, "111"];
+
+  const all = {
+    ...list,
+    ...list2,
+  };
+
+  const x = (name, all) => [...all, { name }];
 
   console.log(x("ura", list));
   console.log(list);
