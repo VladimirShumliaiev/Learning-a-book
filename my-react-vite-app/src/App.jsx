e27d1c904a9eb016015db6f5dc057ec7bea29c81
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-10-15 20:21:26 +0300

    Update App.jsx

--- my-react-vite-app/src/App.jsx
+++ my-react-vite-app/src/App.jsx
@@ -5,12 +5,10 @@ function App() {
 
   const list2 = [{ name: "Igor" }, { name: "Misha" }, "111", "2342"];
 
-  const allList = [...list, ...list2];
+  const all = [...list, ...list2];
 
   const x = (name, all) => [...all, { name }];
 
-  console.log(x("Urasik", allList));
-  console.log(allList);
   return <>O.REilLY-REACT Hello World</>;
 }
 
