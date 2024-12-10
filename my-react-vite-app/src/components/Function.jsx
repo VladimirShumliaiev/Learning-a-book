607162c960baee79d22a22101cbcc050d7fa401a
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-12-10 12:22:56 +0200

    add

--- my-react-vite-app/src/components/Function.jsx
+++ my-react-vite-app/src/components/Function.jsx
@@ -5,8 +5,10 @@ const Function = () => {
     { number: 34 },
     { number: 25 },
     { number: "8 & 9" },
-    { number: 37 },
+    { number: "37" },
   ];
+
+  const a = ["a", "b", "c", "d", "aa", "bb", "cc", "aaa"];
 
   const arr = [{ name: "a" }, { name: "b" }];
   const arr2 = [{ name: "c" }, { name: "d" }, 34];
@@ -14,8 +16,10 @@ const Function = () => {
   const all = [...arr, ...arr2];
 
   const x = (name, allShool) => [...allShool, { name }];
+  const shoolFilter = a.filter((e) => e[0] === "a");
 
   console.log(shool.join(","));
+  console.log(shoolFilter);
   console.log(x("ura", all));
   console.log(x("ura", arr));
   console.log(x("ura", arr2));
