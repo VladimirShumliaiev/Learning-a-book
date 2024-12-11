8ffb113c915818f98c213d3d2597e5a2d2b9aee8
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-12-11 10:10:03 +0200

    Update Function.jsx

--- my-react-vite-app/src/components/Function.jsx
+++ my-react-vite-app/src/components/Function.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
 const Function = () => {
-  const shool = [
-    { number: 34 },
-    { number: 25 },
-    { number: "8 & 9" },
-    { number: "37" },
-  ];
-
-  const a = ["1", "2", "3", "4", "5", "6", "55.346", "5abcd"];
-
-  const arr = [{ name: "a" }, { name: "b" }];
-  const arr2 = [{ name: "c" }, { name: "d" }, 34];
-
-  const all = [...arr, ...arr2];
-
-  const x = (name, allShool) => [...allShool, { name }];
-  const shoolFilter = a.filter((e) => e[0] === "5");
-
-  const cutNumber = (cut, list) => list.filter((e) => e !== cut);
-  console.log(cutNumber("5", a).join(","));
-  console.log(shool.join(","));
-  console.log(shoolFilter);
-  console.log(x("ura", all));
-  console.log(x("ura", arr));
-  console.log(x("ura", arr2));
+  const arr = [{ name: "Borya" }, { name: "Vasya" }, { name: "Ura" }];
 
   return <div></div>;
 };
