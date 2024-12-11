9b3e3b23580183c80005c507dba4bf547f323dad
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2024-12-11 10:16:30 +0200

    add

--- my-react-vite-app/src/components/Array.jsx
+++ my-react-vite-app/src/components/Array.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Array = () => {
-  return <div></div>;
+  return <div>f</div>;
 };
 
 export default Array;
--- my-react-vite-app/src/components/Function.jsx
+++ my-react-vite-app/src/components/Function.jsx
@@ -3,6 +3,10 @@ import React from "react";
 const Function = () => {
   const arr = [{ name: "Borya" }, { name: "Vasya" }, { name: "Ura" }];
 
+  const addArr = (name, list) => [...list, { name }];
+  console.log(arr);
+  console.log(addArr("kolya", arr));
+
   return <div></div>;
 };
 
