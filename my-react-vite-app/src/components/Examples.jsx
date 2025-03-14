7a2d3f37d8560dceb797ab805612af5f29caa022
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-14 20:28:28 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -8,7 +8,16 @@ const Examples = () => {
     { city: "London" },
   ];
 
+  const arr2 = [{ city: "Stambul" }, { city: "Odessa" }];
+
+  const allArr = [...arr, ...arr2];
+
+  const ArrFilter = allArr.filter((e) => e.city[0] !== "P");
+
   console.log(arr);
+  console.log(arr2);
+  console.log(allArr);
+  console.log(ArrFilter);
 
   return <div></div>;
 };
