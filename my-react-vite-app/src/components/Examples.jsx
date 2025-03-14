7a7bf25f4b6aa35b857d0e7a24e144c58d02b25e
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-14 20:07:20 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -5,9 +5,19 @@ const Examples = () => {
     { city: "Kyev ," },
     { city: "Rome ," },
     { city: "Paris ," },
-    { city: "London," },
+    { city: "London, " },
   ];
-  return <div>{arr.map((e) => e.city)}</div>;
+
+  const arr2 = [{ city: "newYork ," }, { city: "Stambul" }];
+
+  const all = [...arr, ...arr2];
+  return (
+    <div>
+      {all.map((e) => (
+        <div key={e.city}>e.city</div>
+      ))}
+    </div>
+  );
 };
 
 export default Examples;
