bfbc1393d5078fc1202d8f200c7608742f0bec84
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-17 09:38:05 +0200

    add

--- my-react-vite-app/src/components/Quiz/Quiz.tsx
+++ my-react-vite-app/src/components/Quiz/Quiz.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react'
+import Game from './Game'
+import Result from './Result'
+import questionList from './questionList'
 
 const Quiz = () => {
   const [step, setStep] = useState(0)
   const [correct, setCorrect] = useState(0)
+
+  const onClickVariant = (index: number) => {
+      setStep(step + 1)
+
+      if( index === question.correct) {
+        setCorrect(correct + 1)
+      }
+
+  }
+
+  const question = questionList[step]
   return (
     <div>
-      <Game />
+       { step !== questionList.length ? <Game onClickVariant={onClickVariant} question={question} /> : <Result correct={correct}/> }
     </div>
   )
 }
--- my-react-vite-app/src/components/Quiz/Result.tsx
+++ my-react-vite-app/src/components/Quiz/Result.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { FC } from 'react'
+import questionList from './questionList'
 
-const Result = () => {
+type ResultProps = {
+  correct: number
+}
+
+const Result: FC<ResultProps> = (props) => {
+  const { correct } = props
   return (
     <div>
-      
+      if (correct < 1) {
+        <h3>you lost</h3>
+      } else {
+        <h3>  you answered {correct} of {questionList.length} questions</h3>
+      }
+     
+
+      <a href="/">new Game</a>
     </div>
   )
 }
