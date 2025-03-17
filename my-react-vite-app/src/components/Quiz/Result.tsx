437c08b891630e711c251b831bd30f28e2b03463
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-17 09:41:07 +0200

    Update Result.tsx

--- my-react-vite-app/src/components/Quiz/Result.tsx
+++ my-react-vite-app/src/components/Quiz/Result.tsx
@@ -7,14 +7,11 @@ type ResultProps = {
 
 const Result: FC<ResultProps> = (props) => {
   const { correct } = props
+
+ 
   return (
     <div>
-      if (correct < 1) {
-        <h3>you lost</h3>
-      } else {
-        <h3>  you answered {correct} of {questionList.length} questions</h3>
-      }
-     
+      <h3>  you answered {correct} of {questionList.length} questions</h3>
 
       <a href="/">new Game</a>
     </div>
