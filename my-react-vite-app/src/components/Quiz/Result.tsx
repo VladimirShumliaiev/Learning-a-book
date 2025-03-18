be46bce1c9da3d6da3081b098d12946d10d4a898
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-18 21:05:01 +0200

    Update Result.tsx

--- my-react-vite-app/src/components/Quiz/Result.tsx
+++ my-react-vite-app/src/components/Quiz/Result.tsx
@@ -10,7 +10,7 @@ const Result: FC<ResultProps> = (props) => {
 
   return (
     <div>
-      <h3> you answered {correct} of {questionList.length} questions</h3>
+      <h3> you answered {correct} of {questionList.length} questions!</h3>
 
       <a href="/">new Game</a>
     </div>
