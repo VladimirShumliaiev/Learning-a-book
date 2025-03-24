5b10165f3e69ae643bd8ed4bf970ef84fa063775
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-24 20:28:04 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -1,29 +1,8 @@
 import React from "react";
 
 const Examples = () => {
-  const cities = [
-    { name: "Kiev" },
-    { name: "London" },
-    { name: "Rome" },
-    { name: "Paris" },
-    { name: "Madrid" },
-  ];
-
-  const cities2 = [{ name: "Milan" }, { name: "Odessa" }];
-
-  const allCities = [...cities, ...cities2];
-
-  const filterCities = allCities.filter((e) => e.name[0] !== "M");
-
-  const cutCities = (cut, citiesList) =>
-    citiesList.filter((e) => e.name !== cut);
-
-  console.log(cities);
-  console.log(cities2);
-  console.log(allCities);
-  console.log(filterCities);
-  console.log(cutCities("Paris", allCities));
-  return <h1>{cities.map((e) => e.name).join(",")}</h1>;
+  const names = [{ name: "Ura" }, { name: "Kolya" }, { name: "Petya" }];
+  return <div>{names[0]}</div>;
 };
 
 export default Examples;
