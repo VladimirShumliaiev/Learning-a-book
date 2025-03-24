a95f7c5024c73088af55506bd5dacbcb9dd43295
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-24 21:17:21 +0200

    Update Function.jsx

--- my-react-vite-app/src/components/Function.jsx
+++ my-react-vite-app/src/components/Function.jsx
@@ -14,11 +14,14 @@ const Function = () => {
   });
 
   const shool = [34, 25, 37];
+  const shool2 = [{ num: 34 }, { num: 25 }, { num: 37 }];
 
   const cutShool = (cut, list) => list.filter((e) => e !== cut);
+  const shoolFilter = shool2.filter((e) => e.num[0] !== "3");
 
   console.log(selfEducate(frederick));
   console.log(frederick);
+  console.log(shoolFilter);
 
   const Header = (props) => <h1>{props}</h1>;
   return (
@@ -27,6 +30,8 @@ const Function = () => {
       {cutShool(25, shool).join(",")}
       <br />
       {shool.join("\n")}
+      <br />
+      {shoolFilter.num}
     </div>
   );
 };
