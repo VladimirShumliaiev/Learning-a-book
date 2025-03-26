13d3f6360a881f519840eb9a335468328b5ac5da
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-26 20:46:22 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -11,12 +11,14 @@ const Examples = () => {
 
   const cityAll = [...city, ...city2];
 
-  const cityFilter = cityAll.filter((city) => city.cityName[0] === "P");
+  const cityFilter = cityAll.filter((city) => city.cityName[0] !== "P");
+  const cutCity = (cut, list) => list.filter((e) => e.cityName !== cut);
 
   console.log(city);
   console.log(city2);
   console.log(cityAll);
   console.log(cityFilter);
+  console.log(cutCity("London", cityAll));
   return <div></div>;
 };
 
