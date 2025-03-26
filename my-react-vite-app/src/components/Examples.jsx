7cbe59f4c91e5eab8b8ba729bb38a7bd7ccef760
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-26 20:40:57 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -7,7 +7,13 @@ const Examples = () => {
     { cityName: "Odessa" },
   ];
 
+  const city2 = [{ cityName: "New York" }, { cityName: "Paris" }];
+
+  const cityAll = [...city, ...city2];
+
   console.log(city);
+  console.log(city2);
+  console.log(cityAll);
   return <div></div>;
 };
 
