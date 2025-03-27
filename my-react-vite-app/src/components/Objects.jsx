a3fb2ec20f17cfafa527c227ac8a7a1187f8561f
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-03-27 20:37:30 +0200

    Update Objects.jsx

--- my-react-vite-app/src/components/Objects.jsx
+++ my-react-vite-app/src/components/Objects.jsx
@@ -4,7 +4,7 @@ const Objects = () => {
   const onClickHandle = () => {};
   return (
     <div>
-      <button onClick={onClickHandle}>add</button>
+      <button onClick={onClickHandle}>add todo</button>
     </div>
   );
 };
