c3d2a86c475df02d448051bcbdd58fda51268511
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-09 19:27:50 +0300

    add

--- my-react-vite-app/src/components/Test.jsx
+++ my-react-vite-app/src/components/Test.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
+import styles from './Test'
 
 const Test = () => {
   const [state, setState] = useState(0);
 
   const onClickPlus = () => {
     setState(state + 1);
+
+    if (state >= 5) {
+      setState(0);
+    }
   };
 
   const onClickMinus = () => {
     setState(state - 1);
+
+    if (state <= -5) {
+      setState(0);
+    }
   };
   return (
-    <div>
-      <button onClick={onClickPlus}> Plus </button>
-      {state}
+    <div className={}>
+      <button onClick={onClickPlus}> Plus </button> {state}{" "}
       <button onClick={onClickMinus}> Minus </button>
     </div>
   );
