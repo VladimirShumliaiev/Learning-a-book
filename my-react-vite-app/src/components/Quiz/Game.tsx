4a99a50d718eea4b576e0d2a032a50825d689da5
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-09 18:55:43 +0300

    add

--- my-react-vite-app/src/components/Quiz/Game.tsx
+++ my-react-vite-app/src/components/Quiz/Game.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const Game = () => {
+type Question = {
+  id: number
+  title: string
+  variants: string[]
+  correct: number
+}
+
+type GameProps = {
+  question: Question,
+  onClickVariant: (index: number) => void
+}
+
+const Game: FC<GameProps> = (props) => {
+  const {question, onClickVariant} = props
   return (
     <div>
-      
+        <h3>{question.title}</h3>
+        {question.variants.map((e, index) => <li key={index}><button onClick={() => onClickVariant(index)}>{e}</button></li>)}
     </div>
   )
 }
--- my-react-vite-app/src/components/Quiz/Quiz.tsx
+++ my-react-vite-app/src/components/Quiz/Quiz.tsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
+import questionList from './questionList'
+import Game from './Game'
+import Result from './Result'
 
 const Quiz = () => {
   const [step ,setStep] = useState(0)
-  const [variant, setVariant] = useState(0)
+  const [correct, setCorrect] = useState(0)
 
-  const onClickVariant = 
+  const onClickVariant = (index: number) => {
+    setStep(step + 1)
+
+    if (index === question.correct) {
+      setCorrect(correct + 1)
+    }
+  }
+
+  const question = questionList[step]
+
+
 
 
   return (
     <div>
-      
+        {step !== questionList.length ? <Game question={question} onClickVariant={onClickVariant}/> : <Result correct={correct}/>}
     </div>
   )
 }
