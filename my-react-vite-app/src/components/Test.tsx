4c4ba03c8eb6d034dec2ee8657808465f821507c
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-10 19:34:36 +0300

    Update Test.tsx

--- my-react-vite-app/src/components/Test.tsx
+++ my-react-vite-app/src/components/Test.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 
 const Test = () => {
   const [counter, setCounter] = useState(0)
+  const [color, setColor] = useState('')
 
   const handlePlus = () => {
     setCounter(counter + 1)
@@ -26,6 +27,15 @@ const Test = () => {
         <button onClick={handlePlus}>+</button>{' '}
         {counter}
         {' '}<button onClick={handleMinus}>-</button>
+
+        <hr />
+          <h3>Colors: </h3>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+
     </div>
   )
 }
