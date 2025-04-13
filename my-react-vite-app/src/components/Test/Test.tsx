5452bc037ef3a945f46370ebbfed43b15bcbcfb7
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-13 21:24:35 +0300

    Update Test.tsx

--- my-react-vite-app/src/components/Test/Test.tsx
+++ my-react-vite-app/src/components/Test/Test.tsx
@@ -38,10 +38,10 @@ const Test = () => {
         <hr />
           <h3>Colors: </h3>
         <button className={ color === '1' ? 'one-color' : ''} onClick={() => handleOnClickColor('1')}>one</button>
-        <button className={ color === '2' ? 'one-color' : ''} onClick={() => handleOnClickColor('2')}>two</button>
-        <button className={ color === '3' ? 'one-color' : ''} onClick={() => handleOnClickColor('3')}></button>
-        <button className={ color === '4' ? 'one-color' : ''} onClick={() => handleOnClickColor('4')}></button>
-        <button className={ color === '5' ? 'one-color' : ''} onClick={() => handleOnClickColor('5')}></button>
+        <button className={ color === '2' ? 'two-color' : ''} onClick={() => handleOnClickColor('2')}>two</button>
+        <button className={ color === '3' ? 'three-color' : ''} onClick={() => handleOnClickColor('3')}>three</button>
+        <button className={ color === '4' ? 'four-color' : ''} onClick={() => handleOnClickColor('4')}>four</button>
+        <button className={ color === '5' ? 'five-color' : ''} onClick={() => handleOnClickColor('5')}>five</button>
         <hr />
     </div>
   )
