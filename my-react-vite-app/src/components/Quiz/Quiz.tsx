8d9b5d4d50a473732d4b98d6d13f4965dbc959cf
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-14 20:34:37 +0300

    Update Quiz.tsx

--- my-react-vite-app/src/components/Quiz/Quiz.tsx
+++ my-react-vite-app/src/components/Quiz/Quiz.tsx
@@ -1,8 +1,21 @@
 import React, { useState } from 'react'
+import questionList from './questionList'
 
 const Quiz = () => {
   const [step, setStep] = useState(0)
   const [correct, setCorrect] = useState(0)
+
+  const question = questionList[step]
+
+  const handleOnClickButton = (value: number) => {
+      setStep(step + 1)
+
+      if (value === question.correct) {
+        setCorrect(correct + 1)
+      }
+  }
+
+  
   return (
     <div>
       
