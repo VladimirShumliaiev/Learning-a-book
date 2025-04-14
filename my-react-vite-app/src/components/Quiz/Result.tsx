977590c0490ad89b3fdbefde57d635bc2173d44c
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-14 20:46:36 +0300

    Update Result.tsx

--- my-react-vite-app/src/components/Quiz/Result.tsx
+++ my-react-vite-app/src/components/Quiz/Result.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import questionList from './questionList'
 
 type ResultProps = {
   correct: number
@@ -8,7 +9,8 @@ const Result: FC<ResultProps> = (props) => {
     const {correct} = props
   return (
     <div>
-        <h3></h3>
+        <h3>you answered {correct} of {questionList.length} questions</h3>
+        <a href="/">New Game</a>
     </div>
   )
 }
