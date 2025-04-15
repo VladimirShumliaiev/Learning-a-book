060a8c34131e0bb0ae805201d020aa034f019ece
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-15 11:17:37 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Examples.jsx
+++ my-react-vite-app/src/components/Examples.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const Examples = () => {
+  const nameList = [
+    { name: "Olya" },
+    { name: "Olya" },
+    { name: "Olesya" },
+    { name: "Oksana" },
+  ];
+
+  const nameListTwo = [{ name: "Ira" }, { name: "Ina" }, { name: "Katya" }];
+
+  const nameListAll = [...nameList, ...nameListTwo];
+
+  const nameFilter = nameListAll.filter((e) => e.name[0] !== "K");
+
+  const cutName = (cut, name) => console.log(nameList);
+  console.log(nameListTwo);
+  console.log(nameListAll);
+  console.log(nameFilter);
+
   return <div></div>;
 };
 
