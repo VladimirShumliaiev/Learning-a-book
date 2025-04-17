32feffdbeb0e031a3fc289c8382ae8896941b749
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-17 16:25:41 +0300

    add

--- my-react-vite-app/src/components/NavBar/NavBar.jsx
+++ my-react-vite-app/src/components/NavBar/NavBar.jsx
@@ -3,37 +3,26 @@ import { NavLink } from "react-router";
 import style from "./NavBar.module.css";
 
 const NavBar = () => {
+  const isActive = ({ isActive }) => (isActive ? "item-active-link" : "");
   return (
-    <div>
+    <div className={style.item}>
       <div>
-        <NavLink className={style.item} to={"quiz"}>
+        <NavLink className={isActive} to={"quiz"}>
           {" "}
           Quiz
         </NavLink>
       </div>
       <div>
-        <NavLink className={style.item} to={"test"}>
-          {" "}
-          Test
-        </NavLink>
+        <NavLink to={"test"}> Test</NavLink>
       </div>
       <div>
-        <NavLink className={style.item} to={"object"}>
-          {" "}
-          Object
-        </NavLink>
+        <NavLink to={"object"}> Object</NavLink>
       </div>
       <div>
-        <NavLink className={style.item} to={"function"}>
-          {" "}
-          Function
-        </NavLink>
+        <NavLink to={"function"}> Function</NavLink>
       </div>
       <div>
-        <NavLink className={style.item} to={"clock"}>
-          {" "}
-          Clock
-        </NavLink>
+        <NavLink to={"clock"}> Clock</NavLink>
       </div>
       <div>
         <NavLink className={style.item} to={"example"}>
