1ab14ba413a1a482bbe91b773c83f46c2999de69
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-04-30 13:37:53 +0300

    add

--- my-react-vite-app/src/components/Recipes/Instructions.jsx
+++ my-react-vite-app/src/components/Recipes/Instructions.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import style from "./Menu.module.css";
 
-const Instructions = ({ title, instructions }) => {
+const Instructions = ({ title, list }) => {
   return (
     <div className={style.instructions}>
       <h3>{title}</h3>
-      {instructions.map((instructions, i) => (
+      {list.map((instructions, i) => (
         <p key={i}>{instructions}</p>
       ))}
     </div>
