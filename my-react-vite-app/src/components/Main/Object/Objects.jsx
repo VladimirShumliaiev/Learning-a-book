b68b35bb17ef68004625bb293adb9a1091a813e1
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-01 11:31:26 +0300

    add

--- my-react-vite-app/src/components/Main/Object/Objects.jsx
+++ my-react-vite-app/src/components/Main/Object/Objects.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Objects = () => {
-  const onClickHandle = () => {};
+  const [text, setText] = useState("");
+  const [add, setAdd] = useState("");
+
+  const onClickHandle = () => {
+    setAdd(text);
+    setText("");
+  };
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
+      <input onChange={handleOnChange} value={text} />
       <button onClick={onClickHandle}>add </button>
+      <div>{add}</div>
       <a href="/">back to homePage</a>
     </div>
   );
