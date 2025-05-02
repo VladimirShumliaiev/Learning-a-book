a9d5a3c82f90fe9642d6090302401357ebfc4819
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-02 09:05:31 +0300

    add

--- my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
+++ my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
-import Star from './Star'
+import React, { useState } from "react";
+import Star from "./Star";
 
-const StarRating = ({totalStar = 5}) => {
-    const [selected,setSelected] = useState(0)
+const StarRating = ({ totalStar = 5 }) => {
+  const [selected, setSelected] = useState(0);
 
-    const newArray = (length) => [...Array(length)]
-
+  const createArray = (length) => [...Array(length)];
   return (
     <>
-        <h2>
-        {
-            newArray(totalStar).map((n,i) => <Star key={i} selected={selected > i} onSelect={() => setSelected(i + 1)}/>)
-        }
+      <h2>
+        {createArray(totalStar).map((n, i) => (
+          <Star
+            key={i}
+            selected={selected > i }
+            onSelected={() => setSelected(i + 1)}
+          />
+        ))}
 
-          <p>
-            {selected} of {totalStar} star
-        </p>
-        </h2>
-
-      
-       
+        <div>
+          {selected} of {totalStar} star
+        </div>
+      </h2>
     </>
-  )
-}
+  );
+};
 
-export default StarRating
+export default StarRating;
