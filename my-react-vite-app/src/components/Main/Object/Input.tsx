95fb54fe06ada6689995eb0ec81dfd4e48e8f5b0
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-09 18:47:06 +0300

    add

--- my-react-vite-app/src/components/Main/Object/Input.tsx
+++ my-react-vite-app/src/components/Main/Object/Input.tsx
@@ -1,15 +1,24 @@
 import React from "react";
+import Button from "./Button";
 
 
 
-const Input = ({ title, setTitle }) => {
+const Input = ({ title, setTitle,display, setDisplay}) => {
   const handleOnChange = (e) => {
     setTitle(e.target.value);
   };
+
+  const handleOnSubmit = (e) => {
+    e.prevent.Default
+    setDisplay(title)
+  }
   return (
-    <>
+    <form onSubmit={handleOnSubmit}>
       <input type="text" value={title} onChange={handleOnChange} />
-    </>
+       <Button title={title} setDisplay={setDisplay} setText={setTitle}>
+        add Title
+      </Button>
+    </form>
   );
 };
 
