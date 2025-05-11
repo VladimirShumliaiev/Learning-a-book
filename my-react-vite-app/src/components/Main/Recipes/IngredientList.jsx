fc4163c6c239d96f96b7c55f5989334e94b9cebd
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-11 11:17:19 +0300

    add

--- my-react-vite-app/src/components/Main/Recipes/IngredientList.jsx
+++ my-react-vite-app/src/components/Main/Recipes/IngredientList.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Ingredients from "./Ingredients";
 
-const IngredientList = ({ list }) => {
+const IngredientList = ({ list, title }) => {
   return (
     <div>
+      <h2>{title}</h2>
       <ul>
         {list.map((ingredients, i) => (
           <Ingredients key={i} {...ingredients} />
