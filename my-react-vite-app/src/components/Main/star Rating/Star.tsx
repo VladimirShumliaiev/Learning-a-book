bf74a8491467477314061c98f76c539780c4e4f0
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-13 19:14:06 +0300

    add

--- my-react-vite-app/src/components/Main/star Rating/Star.tsx
+++ my-react-vite-app/src/components/Main/star Rating/Star.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { FaStar } from "react-icons/fa";
 
-const Star = () => {
+const Star = ({ selected = false, setSelect = (f) => f }) => {
   return (
     <div>
-      
+      <FaStar color={selected ? "blue" : "grey"} onClick={setSelect} />
     </div>
-  )
-}
+  );
+};
 
-export default Star
+export default Star;
--- my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
+++ my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react'
-import Star from './Star'
+import React, { useState } from "react";
+import Star from "./Star";
 
-const totalStar = 5
+const totalStar = 5;
 
 const StarRating = (totalStar) => {
-  const [selectedStar,setSelectedStar] = useState(0)
-  const createArray = (length) => [...Array(length)]
+  const [selectedStar, setSelectedStar] = useState(0);
+  const createArray = (length) => [...Array(length)];
   return (
     <div>
-        {createArray(totalStar).map((n,i) => <Star key={i} selected={selectedStar} onSelect/>)}
+      <h2>
+        {createArray(totalStar).map((n, i) => (
+          <Star
+            key={i}
+            selected={selectedStar > i}
+            setSelect={() => setSelectedStar(i + 1)}
+          />
+        ))}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default StarRating
+export default StarRating;
