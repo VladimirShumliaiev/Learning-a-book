a8bd970a98c5e0b02e486a415a8e6fa70cd241d0
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-13 19:03:58 +0300

    Update StarRating.tsx

--- my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
+++ my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
+import Star from './Star'
 
 const totalStar = 5
 
 const StarRating = (totalStar) => {
   const [selectedStar,setSelectedStar] = useState(0)
-  const 
+  const createArray = (length) => [...Array(length)]
   return (
     <div>
-
+        {createArray(totalStar).map((n,i) => <Star key={i} selected={selectedStar} onSelect/>)}
     </div>
   )
 }
