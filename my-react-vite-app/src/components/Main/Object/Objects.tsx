c6bb9464ba809f50e2e94d5293d1b38b5f7ed9a4
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-14 17:48:09 +0300

    add

--- my-react-vite-app/src/components/Main/Object/Objects.tsx
+++ my-react-vite-app/src/components/Main/Object/Objects.tsx
@@ -1,1 +1,17 @@
-ra;
+import React, { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
+
+const Objects = () => {
+  const [title, setTitle] = useState("");
+  const [display, setDisplay] = useState("");
+
+  return (
+    <div>
+      <Input title={title} setTitle={setTitle} />
+      <Button title={title}>add</Button>
+    </div>
+  );
+};
+
+export default Objects;
