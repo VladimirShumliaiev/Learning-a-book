ad230e53c139619198b1399361e1a31c3886526e
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-16 10:27:11 +0300

    add

--- my-react-vite-app/src/components/Main/todo/Todo.tsx
+++ my-react-vite-app/src/components/Main/todo/Todo.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useAppDispatch } from "../../hooks/hooks";
+import { fetchTodo } from "../../../redux/Slices/TodoSlice";
+import List from "./List";
+import Input from "./Input";
 
 const Todo = () => {
   const [title, setTitle] = useState("");
   const dispatch = useAppDispatch();
 
-  useEffect(() => {}, []);
-  return <div></div>;
+  useEffect(() => {
+    dispatch(fetchTodo());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Input />
+      <List />
+    </div>
+  );
 };
 
 export default Todo;
