070d1eb058c0bcd50fb64f2e87b43ff231d9baf6
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-16 10:47:37 +0300

    add

--- my-react-vite-app/src/components/Main/todo/Item.tsx
+++ my-react-vite-app/src/components/Main/todo/Item.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { FC } from "react";
+import { useAppDispatch } from "../../hooks/hooks";
+import { deleteTodo, toggleTodo } from "../../../redux/Slices/TodoSlice";
 
-const Item = () => {
-  return <div></div>;
+type ItemProps = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+const Item: FC<ItemProps> = (props) => {
+  const { id, title, completed } = props;
+  const dispatch = useAppDispatch();
+
+  const handlerOnChange = () => {
+    dispatch(toggleTodo(id));
+  };
+
+  const handlerOnClick = () => {
+    dispatch(deleteTodo(id));
+  };
+  return (
+    <div>
+      <input type={"checkbox"} checked={completed} onChange={} />
+      {title}
+      <button onClick={}>X</button>
+    </div>
+  );
 };
 
 export default Item;
