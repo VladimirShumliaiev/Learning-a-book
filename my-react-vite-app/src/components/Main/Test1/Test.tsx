44e91abb91642292d23213f31683c231013e7aec
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-05-18 22:23:37 +0300

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -14,7 +14,7 @@ const Test = () => {
 
   const handlerMinus = () => {
     setCount(count - 1);
-    if (count <= -20) {
+    if (count <= -15) {
       window.confirm("rest 0");
       setCount(0);
     }
