d36c1668ae0476d0329ae1690aff0a20b0faf7bb
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2025-06-17 17:05:48 +0300

    add

--- my-react-vite-app/src/components/Main/star Rating/Star.tsx
+++ my-react-vite-app/src/components/Main/star Rating/Star.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
-const Star = () => {
-  return <div></div>;
+const Star = ({ starRating = false, setStarRating = (f) => f }) => {
+  return (
+    <>
+      {<FaStar color={starRating ? "blue" : "gray"} onClick={setStarRating} />}
+    </>
+  );
 };
 
 export default Star;
--- my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
+++ my-react-vite-app/src/components/Main/star Rating/StarRating.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import Star from "./Star";
 
-const StarRating = () => {
+const StarRating = ({ totalStar = 5 }) => {
+  const [starRating, setStarRating] = useState(0);
+
+  const createArray = (length) => [...Array(length)];
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <h2>
+        {createArray(totalStar).map((n, i) => (
+          <Star
+            key={i}
+            starRating={starRating > i}
+            setStarRating={() => setStarRating(i + 1)}
+          />
+        ))}
+        <div>
+          {starRating} of {totalStar} stars
+        </div>
+      </h2>
+    </>
+  );
+};
 
-export default StarRating
+export default StarRating;
