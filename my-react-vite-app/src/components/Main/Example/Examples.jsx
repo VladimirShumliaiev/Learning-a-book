d8bff1be75c52057fcbdd1cbec1ecc78e7b4e1c7
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-12 11:48:54 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -11,12 +11,13 @@ const Examples = () => {
   const nameListMan = [{ name: "Vasya" }, { name: "Kolya" }, { name: "Petya" }];
   const nameListAll = [...nameList, ...nameListMan];
   const nameListFilter = nameListAll.filter((e) => e.name[1] !== "a");
-  const nameListCut = (cut, list) = nameListAll.filter(e => e.cut)
+  const nameListCut = (cut, list) => list.filter((e) => e.name !== cut);
 
   console.log(nameList);
   console.log(nameListMan);
   console.log(nameListAll);
   console.log(nameListFilter);
+  console.log(nameListCut("Olya", nameListAll));
   return (
     <div>
       <div>
