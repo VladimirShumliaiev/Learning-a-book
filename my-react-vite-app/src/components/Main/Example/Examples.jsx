6c056e08845d0ae5401b3149b7e45ceff325306d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-17 12:26:59 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 
 const Examples = () => {
+  const firstName = [
+    { name: "Bob" },
+    { name: "Oleg" },
+    { name: "Vasya" },
+    { name: "Petya" },
+  ];
+  const firstNameTwo = [
+    { name: "Kolya" },
+    { name: "Ura" },
+    { name: "Maksim" },
+    { name: "Sasha" },
+  ];
+
+  console.log(firstName);
+  console.log(firstNameTwo);
   return <div></div>;
 };
 
