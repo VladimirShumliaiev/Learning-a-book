09ae0be547404f078901557fb4518f391bbbabec
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-17 12:46:58 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -1,65 +1,27 @@
 import React, { useState } from "react";
-import "./Test.css";
 
 const Test = () => {
   const [count, setCount] = useState(0);
-  const [color, setColor] = useState("three");
 
-  const handlerPlus = () => {
-    setCount(count + 1);
-    if (count >= 11) {
-      setCount(0);
+  const plusHandle = () => {
+    setCount(count + 1)
+    if(count >= 10) {
+      window.alert('!')
+      setCount(0)
     }
-  };
 
-  const handlerMinus = () => {
-    setCount(count - 1);
-    if (count <= -11) {
-      window.confirm("rest 0");
-      setCount(0);
+    const minusHandle = () => {
+    setCount(count - 1)
+    if(count >= -10) {
+      window.alert('!')
+      setCount(0)
     }
-  };
-
+  }
   return (
     <div>
-      Counter:
-      <div>
-        <button onClick={handlerPlus}>plus +</button> {count}{" "}
-        <button onClick={handlerMinus}>- minus</button>
-      </div>
-      Color active:
-      <div>
-        <button
-          className={color === "one" ? "one-color" : ""}
-          onClick={() => setColor("one")}
-        >
-          1
-        </button>
-        <button
-          className={color === "two" ? "two-color" : ""}
-          onClick={() => setColor("two")}
-        >
-          2
-        </button>
-        <button
-          className={color === "three" ? "three-color" : ""}
-          onClick={() => setColor("three")}
-        >
-          3
-        </button>
-        <button
-          className={color === "four" ? "four-color" : ""}
-          onClick={() => setColor("four")}
-        >
-          4
-        </button>
-        <button
-          className={color === "five" ? "five-color" : ""}
-          onClick={() => setColor("five")}
-        >
-          5
-        </button>
-      </div>
+      <button> + </button>
+      {count}
+      <button> - </button>
     </div>
   );
 };
