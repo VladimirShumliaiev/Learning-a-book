edf320f71620659c34cd55de2e6cede6b81f84cd
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-17 12:22:57 +0200

    add

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,37 +1,7 @@
 import React from "react";
 
 const Examples = () => {
-  const firstName = [
-    { name: "Oleg" },
-    { name: "Petya" },
-    { name: "Ura" },
-    { name: "Sasha" },
-  ];
-
-  const firstNameTwo = [
-    { name: "Borya" },
-    { name: "Serega" },
-    { name: "Dima" },
-  ];
-
-  const allFirstName = [...firstName, ...firstNameTwo];
-  const nameFilter = allFirstName.filter((e) => e.name[0] === "S");
-  const nameCut = (cut, list) => list.filter((e) => e.name !== cut);
-
-  console.log(firstName);
-  console.log(firstNameTwo);
-  console.log(allFirstName);
-  console.log(nameFilter);
-  console.log(nameCut("Dima", allFirstName));
-
-  return (
-    <div>
-      <pre>{allFirstName.map((e) => e.name).join("\n")}</pre>
-      <div>
-        <a href="/">back to homePage</a>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Examples;
