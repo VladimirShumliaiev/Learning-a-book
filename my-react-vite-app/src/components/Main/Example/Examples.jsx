43f743ced4615ec1db9bc27b22eb372799ffc60c
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-17 12:32:01 +0200

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -14,8 +14,16 @@ const Examples = () => {
     { name: "Sasha" },
   ];
 
+  const allFirstName = [...firstName, ...firstNameTwo];
+  const nameFilter = allFirstName.filter((e) => e.name[0] !== "B");
+  const cutNameFilter = (cut, list) => list.filter((e) => e.name !== cut);
+
   console.log(firstName);
   console.log(firstNameTwo);
+  console.log(allFirstName);
+  console.log(nameFilter);
+  console.log(cutNameFilter("Vasya", allFirstName));
+
   return <div></div>;
 };
 
