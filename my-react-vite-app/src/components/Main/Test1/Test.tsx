ba36f4f88c301d1a9815f9701094dedbaaa89b0d
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-17 12:55:13 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -2,26 +2,36 @@ import React, { useState } from "react";
 
 const Test = () => {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState();
 
   const plusHandle = () => {
-    setCount(count + 1)
-    if(count >= 10) {
-      window.alert('!')
-      setCount(0)
+    setCount(count + 1);
+    if (count >= 10) {
+      window.alert("!");
+      setCount(0);
     }
-
-    const minusHandle = () => {
-    setCount(count - 1)
-    if(count >= -10) {
-      window.alert('!')
-      setCount(0)
+  };
+  const minusHandle = () => {
+    setCount(count - 1);
+    if (count <= -10) {
+      window.alert("!");
+      setCount(0);
     }
-  }
+  };
   return (
     <div>
-      <button> + </button>
-      {count}
-      <button> - </button>
+      <div>
+        <button onClick={plusHandle}> + </button> {count}{" "}
+        <button onClick={minusHandle}> - </button>
+      </div>
+      color:
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </div>
     </div>
   );
 };
