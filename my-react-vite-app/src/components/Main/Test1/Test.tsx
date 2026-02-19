04b3731f4e6476ad4ad5bb9a018f9d1f73f10d0a
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-19 21:24:48 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
+import "./Test.css";
 
 const Test = () => {
   const [color, setColor] = useState("three");
   return (
     <div>
       <button
-        onClick={() => setColor("one")}
         className={color === "one" ? "one-color" : ""}
+        onClick={() => setColor("one")}
       >
         1
       </button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
+      <button
+        className={color === "two" ? "two-color" : ""}
+        onClick={() => setColor("two")}
+      >
+        2
+      </button>
+      <button
+        className={color === "three" ? "three-color" : ""}
+        onClick={() => setColor("three")}
+      >
+        3
+      </button>
+      <button
+        className={color === "four" ? "four-color" : ""}
+        onClick={() => setColor("four")}
+      >
+        4
+      </button>
+      <button
+        className={color === "five" ? "five-color" : ""}
+        onClick={() => setColor("five")}
+      >
+        5
+      </button>
     </div>
   );
 };
