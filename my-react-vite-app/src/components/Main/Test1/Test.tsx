9c9508b20acf10df1fcf0ef4fa565436cded92d7
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-22 18:48:15 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -1,62 +1,17 @@
 import React, { useState } from "react";
-import "./Test.css";
 
 const Test = () => {
-  const [color, setColor] = useState("three");
-  const [count, setCount] = useState(0);
-
-  const plusHandle = () => {
-    setCount(count + 1);
-    if (count >= 5) {
-      setCount(0);
-    }
-  };
-
-  const minusHandle = () => {
-    setCount(count - 1);
-    if (count <= -5) {
-      setCount(0);
-    }
-  };
+  const [color, setColor] = useState("");
 
   return (
     <div>
-      <button
-        className={color === "one" ? "one-color" : ""}
-        onClick={() => setColor("one")}
-      >
-        a
-      </button>
-      <button
-        className={color === "two" ? "two-color" : ""}
-        onClick={() => setColor("two")}
-      >
-        b
-      </button>
-      <button
-        className={color === "three" ? "three-color" : ""}
-        onClick={() => setColor("three")}
-      >
-        c
-      </button>
-      <button
-        className={color === "four" ? "four-color" : ""}
-        onClick={() => setColor("four")}
-      >
-        d
-      </button>
-      <button
-        className={color === "five" ? "five-color" : ""}
-        onClick={() => setColor("five")}
-      >
-        f
-      </button>
-      <div>
-        Count:
-        <button onClick={minusHandle}>-</button> {count}{" "}
-        <button onClick={plusHandle}>+</button>
-      </div>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
     </div>
   );
 };
+
 export default Test;
