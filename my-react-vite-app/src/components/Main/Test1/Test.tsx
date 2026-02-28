11d609db3935f17b5e85386f9155f9256a16b0cc
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-28 21:10:02 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -4,7 +4,8 @@ const Test = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <button>Plus</button> {count} <button> Minus</button>
+      <button>Plus</button> {count}
+      <button> Minus</button>
     </div>
   );
 };
