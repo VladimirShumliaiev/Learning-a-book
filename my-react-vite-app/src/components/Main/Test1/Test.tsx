0d5d566e1937a21f4cabda64f5378512e1c5f66e
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-02-28 21:15:26 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -15,7 +15,14 @@ const Test = () => {
     <div>
       <button onClick={handlePlus}>Plus</button> {count}{" "}
       <button onClick={handleMinus}> Minus</button>
-      <div>color:</div>
+      <div>
+        color:
+        <button className={colorButton === 'a' ? } onClick={() => setColorButton('a')}>a</button>
+        <button  onClick={() => setColorButton('b')}>b</button>
+        <button  onClick={() => setColorButton('c')}>c</button>
+        <button  onClick={() => setColorButton('d')}>d</button>
+        <button  onClick={() => setColorButton('f')}>f</button>
+      </div>
     </div>
   );
 };
