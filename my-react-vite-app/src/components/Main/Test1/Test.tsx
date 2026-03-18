6578abd36547e342996a0498be80365fb8a15e93
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-03-18 18:15:13 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import "./Test.css";
 
 export default function Test() {
   const [count, setCount] = useState(0);
-  const [color, setColor] = useState("");
+  const [color, setColor] = useState("three");
 
   const handlePlus = () => {
     setCount(count + 1);
@@ -28,7 +29,12 @@ export default function Test() {
 
       <div>
         <div>color:</div>
-        <button className={color === "one" ? "one-color" : ""}>1</button>
+        <button
+          className={color === "one" ? "one-color" : ""}
+          onClick={() => setColor("one")}
+        >
+          1
+        </button>
         <button className={color === "two" ? "two-color" : ""}>2</button>
         <button className={color === "three" ? "three-color" : ""}>3</button>
         <button className={color === "four" ? "four-color" : ""}>4</button>
