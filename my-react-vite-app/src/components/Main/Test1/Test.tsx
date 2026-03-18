4698864ddf1b06a3302f27413b27f41606929014
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-03-18 18:05:24 +0200

    Update Test.tsx

--- my-react-vite-app/src/components/Main/Test1/Test.tsx
+++ my-react-vite-app/src/components/Main/Test1/Test.tsx
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 
 export default function Test() {
   const [count, setCount] = useState(0);
+
+  const handlePlus = () => {
+    setCount(count + 1);
+  };
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <button>+</button>
+      {count}
       <button>-</button>
     </div>
   );
