6eb9b82e83b47ed840d8ad6bdd78424ac633c238
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-08-19 18:58:05 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const number = [1, 2, 3, 4, 5];
 
-const newNumber;
-const newNumber.map(number * 2)
-console.log(newNumber)
+console.log(number.map((a) => a * 2));
+
 export default function Examples() {
   return <div>111</div>;
 }
