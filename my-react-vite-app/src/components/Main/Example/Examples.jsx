4b59ab8e9d983e76998cf97b752a1cfaa9cfa07c
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-08-19 18:42:37 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,17 +1,5 @@
 import React from "react";
 
 export default function Examples() {
-  const num = [{ n: 1 }, { n: 2 }, { n: 3 }, { n: 4 }, { n: 5 }];
-  const numTwo = [{ n: "213" }, { n: "212" }, { n: "121" }];
-  const numAll = [...num, ...numTwo];
-
-  const numFilter = numAll.filter((e) => e.n[0] !== "1");
-  const cutNum = (cut, list) => list.filter((e) => e.n !== cut);
-
-  console.log(num);
-  console.log(numTwo);
-  console.log(numAll);
-  console.log(numFilter);
-  console.log(cutNum(5, numAll));
   return <div></div>;
 }
