66af90267f2987b1fba413f0019fa8bce1c29397
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-08-19 19:09:00 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 const number = [1, 2, 3, 4, 5];
+const numbers = [3, 8, 12, 5, 20, 7];
+const name = "Vladimir";
+const city = "Odessa";
+
+const user = {
+  name,
+  age: 38,
+  city,
+};
 
 console.log(number.map((e) => e * 2));
-
+console.log(numbers.filter((e) => e > 10));
+console.log(user);
 export default function Examples() {
   return <div> 111 </div>;
 }
