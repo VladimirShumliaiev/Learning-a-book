a64c28bfa5b899fb067a127b20f30427bfbd1882
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-08-23 21:07:05 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 
-function delay(message, shouldFail) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldFail) {
-        reject(new Error("Oshibka"));
-      } else {
-        resolve(message);
-      }
-    }, 1000);
-  });
-}
+// function delay(message, shouldFail) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldFail) {
+//         reject(new Error("Oshibka"));
+//       } else {
+//         resolve(message);
+//       }
+//     }, 1000);
+//   });
+// }
 
-async function getData() {
+// async function getData() {
+//   try {
+//     const result = await delay("Hello", true);
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// getData();
+
+async function getUser() {
   try {
-    const result = await delay("Hello", true);
-    console.log(result);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/1`,
+    );
+    if (!response.ok) {
+      throw new Error("ERROR!!!");
+    }
+
+    const json = await response.json();
+    console.log(json);
   } catch (error) {
     console.log(error.message);
   }
 }
-
-getData();
-
+getUser();
 export default function Examples() {
   return <div></div>;
 }
