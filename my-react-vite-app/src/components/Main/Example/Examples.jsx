2cea20219ea2709d46dfe48fa17a500bf7a9bff3
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2026-08-23 20:04:42 +0300

    Update Examples.jsx

--- my-react-vite-app/src/components/Main/Example/Examples.jsx
+++ my-react-vite-app/src/components/Main/Example/Examples.jsx
@@ -1,90 +1,18 @@
 import React from "react";
 
-const number = [1, 2, 3, 4, 5];
-const numbers = [3, 8, 12, 5, 20, 7];
-const name = "Vladimir";
-const city = "Odessa";
-
-const user = {
-  name,
-  age: 38,
-  city,
-};
-const users = [
-  { name: "Alex", age: 20 },
-  { name: "Vasya", age: 20 },
-  { name: "Petya", age: 20 },
-];
-
-async function getUser(id) {
-  try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${id}`,
-    );
-    if (!response.ok) {
-      throw new Error("Ошибка HTTP");
-    }
-    const json = await response.json();
-    console.log(json.name);
-  } catch (error) {
-    console.log("", error.message);
-  }
+async function delay() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Hello`);
+    }, 1000);
+  });
 }
 
-async function getUserTwo(id) {
-  try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${id}`,
-    );
-    if (!response.ok) {
-      throw new Error("Ошибка HTTP");
-    }
-    const json = await response.json();
-    return json;
-  } catch (error) {
-    console.log("", error.message);
-  }
-}
-
-async function getUserPosts(userID) {
-  try {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/users/${userID}`,
-    );
-    if (!response.ok) {
-      throw new Error("Error!!!http");
-    }
-    const json = response.json();
-
-    const responsePost = await fetch(
-      `https://jsonplaceholder.typicode.com/users?userID=${json.id}`,
-    );
-    const jsonPost = await responsePost.json();
-    return jsonPost.length;
-  } catch (error) {
-    console.log("Oshibka", error.message);
-  }
-}
-
-async function main() {
-  const result = await getUserPosts(5);
+async function getDate() {
+  const result = await delay();
   console.log(result);
 }
-
-console.log(number.map((e) => e * 2));
-console.log(numbers.filter((e) => e > 10));
-console.log(user);
-console.log(users.map((e) => e.name));
-
-getUser(5);
-
-getUserTwo(5).then((name) => {
-  console.log(name.name);
-});
-main();
-getUserPosts(5).then((result) => {
-  console.log(result);
-});
+getDate();
 
 export default function Examples() {
   return <div></div>;
